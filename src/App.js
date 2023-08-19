7509065891ab3b4c2c52215297e62ffad8a723b8
Sushmit-Bhalothia <sushmitbhalothia@gmail.com>
2023-08-19 12:29:08 +0530

    BASE URL  UPDATED

--- src/App.js
+++ src/App.js
@@ -1,47 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import UserInputModal from './components/UserInputModal';
-import './App.css';
-import HomePage from './components/HomePage';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Footer from './components/Footer';
+import React, { useState, useEffect } from "react";
+import UserInputModal from "./components/UserInputModal";
+import "./App.css";
+import HomePage from "./components/HomePage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Footer from "./components/Footer";
 
 const App = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [storedId, setStoredId] = useState('');
+  const [storedId, setStoredId] = useState("");
   const [jsonData, setJsonData] = useState(null); // State to hold API response
+  const URL = process.env.REACT_APP_BASE_URL;
+
   const fetchData = async (id) => {
     try {
-      const response = await fetch(`https://9e2c-122-15-204-67.ngrok-free.app/api/recommend/${id}`); // Replace API URL
+      const response = await fetch(`${URL}api/recommend/${id}`); // Replace API URL
       const data = await response.json();
-      console.log(data)
-      data.sort((a, b) => b.users_interested - a.users_interested)
+      console.log(data);
+      data.sort((a, b) => b.users_interested - a.users_interested);
 
       setJsonData(data);
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
   };
   useEffect(() => {
-    const id = localStorage.getItem('id');
+    const id = localStorage.getItem("id");
     if (!id) {
       setIsModalOpen(true);
     } else {
       setStoredId(id);
-      fetchData(id)
+      fetchData(id);
 
       // API call to fetch JSON data, only if id is present
-    
     }
   }, []);
 
   const handleModalSubmit = async (age, gender) => {
     // Make API call
-    const response = await fetch(`https://9e2c-122-15-204-67.ngrok-free.app/api/create-user/?age=${age}&gender=${gender}`);
+    const response = await fetch(
+      `${URL}api/create-user/?age=${age}&gender=${gender}`
+    );
     const data = await response.json();
 
     // Store the ID in local storage
-    localStorage.setItem('id', data.id);
+    localStorage.setItem("id", data.id);
     fetchData(data.id);
     // Close the modal
     setIsModalOpen(false);
@@ -49,7 +52,7 @@ const App = () => {
   };
 
   return (
-    <div className='overflow-hidden'>
+    <div className="overflow-hidden">
       {/* Your app content */}
       {storedId ? (
         <HomePage RecommendedItems={jsonData} />
@@ -61,7 +64,7 @@ const App = () => {
         />
       )}
       <ToastContainer />
-      <Footer/>
+      <Footer />
     </div>
   );
 };
--- src/components/UserInputModal.js
+++ src/components/UserInputModal.js
@@ -1,63 +1,66 @@
-import React, { useState } from 'react';
-import Modal from 'react-modal';
+import React, { useState } from "react";
+import Modal from "react-modal";
 
 const UserInputModal = ({ isOpen, onClose, onSubmit }) => {
-  const [age, setAge] = useState('');
-  const [gender, setGender] = useState('');
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
 
   const handleGenderChange = (event) => {
     setGender(event.target.value);
   };
 
   const handleSubmit = () => {
+    console.log("submitted");
     onSubmit(age, gender);
   };
 
   const modalStyles = {
     overlay: {
-      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      backgroundColor: "rgba(0, 0, 0, 0.5)",
     },
     content: {
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
-      width: '400px',
-      padding: '20px',
-      backgroundColor: 'white',
-      borderRadius: '5px',
-      boxShadow: '0px 2px 10px rgba(0, 0, 0, 0.2)',
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      width: "400px",
+      padding: "20px",
+      backgroundColor: "white",
+      borderRadius: "5px",
+      boxShadow: "0px 2px 10px rgba(0, 0, 0, 0.2)",
     },
   };
 
   const inputStyles = {
-    display: 'block',
-    width: '100%',
-    padding: '8px',
-    marginBottom: '10px',
-    border: '1px solid #ccc',
-    borderRadius: '4px',
+    display: "block",
+    width: "100%",
+    padding: "8px",
+    marginBottom: "10px",
+    border: "1px solid #ccc",
+    borderRadius: "4px",
   };
 
   const labelStyles = {
-    display: 'block',
-    marginBottom: '5px',
-    fontWeight: 'bold',
+    display: "block",
+    marginBottom: "5px",
+    fontWeight: "bold",
   };
 
   const buttonStyles = {
-    display: 'block',
-    width: '100%',
-    padding: '10px',
-    backgroundColor: '#007bff',
-    color: 'white',
-    border: 'none',
-    borderRadius: '4px',
-    cursor: 'pointer',
+    display: "block",
+    width: "100%",
+    padding: "10px",
+    backgroundColor: "#007bff",
+    color: "white",
+    border: "none",
+    borderRadius: "4px",
+    cursor: "pointer",
   };
 
   return (
     <Modal isOpen={isOpen} onRequestClose={onClose} style={modalStyles}>
-      <h2 style={{fontSize:'30px',fontWeight:'bold'}}>Enter Your Information</h2>
+      <h2 style={{ fontSize: "30px", fontWeight: "bold" }}>
+        Enter Your Information
+      </h2>
       <div>
         <input
           type="text"
@@ -68,13 +71,19 @@ const UserInputModal = ({ isOpen, onClose, onSubmit }) => {
         />
         <label style={labelStyles}>
           Gender:
-          <select value={gender} onChange={handleGenderChange} style={inputStyles}>
+          <select
+            value={gender}
+            onChange={handleGenderChange}
+            style={inputStyles}
+          >
             <option value="">Select</option>
             <option value="M">Male</option>
             <option value="F">Female</option>
           </select>
         </label>
-        <button onClick={handleSubmit} style={buttonStyles}>Submit</button>
+        <button onClick={handleSubmit} style={buttonStyles}>
+          Submit
+        </button>
       </div>
     </Modal>
   );
